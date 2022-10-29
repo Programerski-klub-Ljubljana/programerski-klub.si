02f74c3f3fc3bd302fa8e1c14bbbe3ab92bb5291
urosjarc <jar.fmf@gmail.com>
2022-10-29 11:06:18 +0200

    Adding material for clani, oddelki.

---
+++ src/routes/clani/urosjarc/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {Col, Container, Figure, Icon, Image, ListGroup, ListGroupItem, Row} from "sveltestrap";
+</script>
+
+<Container>
+    <Row>
+        <Col md=6>
+            <a href="/clani/urosjarc">
+                <Image fluid alt="Landscape" src="/clani/urosjarc.png"/>
+            </a>
+        </Col>
+        <Col md=6>
+            <h1>Uroš Jarc</h1>
+            <p>Ustanovitelj kluba in trenutno aktivni predsednik kluba.</p>
+
+            <ListGroup id="list">
+                <ListGroupItem><Icon name="calendar3"/> 24.5.1992</ListGroupItem>
+                <ListGroupItem><Icon name="telephone"/> 051-240-885</ListGroupItem>
+                <ListGroupItem><Icon name="envelope"/> jar.fmf@gmail.com</ListGroupItem>
+                <ListGroupItem><Icon name="github"/> urosjarc</ListGroupItem>
+            </ListGroup>
+        </Col>
+    </Row>
+    <hr>
+    <p>
+
+    </p>
+</Container>
--- src/routes/oddelki/trenerski/+page.svelte
+++ src/routes/oddelki/trenerski/+page.svelte
@@ -1,1 +1,34 @@
-Trenerski oddelek.
+<script>
+    import {Col, Container, Figure, Image, Row} from "sveltestrap";
+</script>
+
+<p>
+    V trenerskem oddelku se nahajajo vsi člani, ki so dosegli trenerski status po <a href="/vpis/trener">uspešno opravljenih nalog in preizkušnjah</a>.
+    Vse trenerje kar nekaj časa preverja osebno naš predsednik kluba da vam lahko zagotovimo najvišji standard ki ga premore klub.
+</p>
+
+<p>
+    Na vsakega trenerja smo zelo ponosni in nam je v čast da Vam lahko predstavimo trenutno aktivne trenerje ter Vas povabimo da si preberite njihove
+    zgodbe...
+</p>
+<p>
+    <b>
+        PS: Zaradi ustanovitve kluba ki se je zgodil predkratkim (23.10.2022), je trenutno edini aktivni trener naš predsednik osebno. Aktivno se trudimo
+        iskanja novih trenerjev in njihovih izobraževanj da bomo lahko v prihodnosti nudili večjo raznovrstnot kar se tiče trenerjev. Ob tem naj
+        izkoristimo priložnost in povabimo vsakega ki meni da ima odlično programersko znanje, da se nam pridruži. :)
+    </b>
+</p>
+
+<hr>
+
+<Container>
+    <Row>
+        <Col sm=4>
+            <a href="/clani/urosjarc">
+                <Figure caption="Uroš Jarc">
+                    <Image fluid alt="Landscape" src="/clani/urosjarc.png"></Image>
+                </Figure>
+            </a>
+        </Col>
+    </Row>
+</Container>
