31b389f1436c58dc34556c75683b3cf0313f208f
urosjarc <jar.fmf@gmail.com>
2022-11-03 13:43:58 +0100

    SEO.

---
+++ sitemap.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+routes = ''
+root = Path('src/routes')
+for name in root.rglob('+page.svelte'):
+    name = str(name).replace('/+page.svelte', '')
+    route = str(Path(name).relative_to(root))
+    route = route.replace('.', '')
+
+    routes += f"""
+    <url>
+        <loc>https://programerski-klub.si/{route}</loc>
+    </url>
+    """
+
+
+sitemap =f'''
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+{routes}
+</urlset>
+'''
+
+with open('static/sitemap.xml', 'w') as f:
+    f.write(sitemap)
