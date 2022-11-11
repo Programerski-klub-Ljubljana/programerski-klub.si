bc13a0f27ad29e9a8ffaa28d073fb2c22101138a
urosjarc <jar.fmf@gmail.com>
2022-11-11 16:25:45 +0100

    Fix sitemap, fix login and testing.

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,1 +1,10 @@
+<script>
+    import {UserApi} from "../../api";
+    import * as utils from "../../utils";
+
+    console.log('GUARD')
+    UserApi.info().then((user) => {
+        alert(JSON.stringify(user, null, 4));
+    }).catch(() => utils.route('/admin/login'));
+</script>
 <slot></slot>
